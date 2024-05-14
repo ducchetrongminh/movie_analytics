890aa39c0b640b9c9b4737e2569083178e259bc5
Duc Che <ducchetrongminh@gmail.com>
2024-05-14 22:26:32 +0700

    rename

--- models/base/imdb/base_dim_imdb_movie.sql
+++ models/base/imdb/base_dim_imdb_movie.sql
@@ -1,2 +1,24 @@
+WITH source AS (
+  SELECT *
+  FROM {{ source('imdb', 'title_basics') }}
+)
+
+, rename_column AS (
+  SELECT 
+    -- KEY
+    tconst AS imdb_movie_id
+    -- NAME
+    , primary_title AS title
+    -- DIMENSION
+    , title_type
+    , genres AS imdb_genres
+    -- DATETIME
+    , start_year
+    , end_year
+    -- NUMBER ATTRIBUTES
+    , runtime_minutes
+  FROM source
+)
+
 SELECT *
-FROM {{ source('imdb', 'title_basics') }}
+FROM rename_column
