3a3eac22add821d72142fa2496b1b8c930861092
Duc Che <ducchetrongminh@gmail.com>
2024-05-14 22:27:52 +0700

    cast type

--- models/base/imdb/base_dim_imdb_movie.sql
+++ models/base/imdb/base_dim_imdb_movie.sql
@@ -20,5 +20,17 @@ WITH source AS (
   FROM source
 )
 
+, cast_type AS (
+  SELECT 
+      CAST(imdb_movie_id AS STRING) AS imdb_movie_id
+      , CAST(title AS STRING) AS title
+      , CAST(title_type AS STRING) AS title_type
+      , CAST(imdb_genres AS STRING) AS imdb_genres
+      , CAST(start_year AS INTEGER) AS start_year
+      , CAST(end_year AS INTEGER) AS end_year
+      , CAST(runtime_minutes AS INTEGER) AS runtime_minutes
+  FROM rename_column
+)
+
 SELECT *
-FROM rename_column
+FROM cast_type
