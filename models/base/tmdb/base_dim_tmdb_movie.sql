5439d8d13d6d0b1ff79b56683a4d0a836d25df73
Duc Che <ducchetrongminh@gmail.com>
2024-05-14 21:58:27 +0700

    rename col

--- models/base/tmdb/base_dim_tmdb_movie.sql
+++ models/base/tmdb/base_dim_tmdb_movie.sql
@@ -1,2 +1,24 @@
+WITH source AS (
+  SELECT *
+  FROM {{ source('tmdb', 'tmdb_movie_dataset') }}
+)
+
+, rename_column AS (
+  SELECT 
+    id AS tmdb_movie_id
+    , title AS tmdb_movie_title
+    , vote_average AS tmdb_vote_avg
+    , vote_count AS tmdb_vote_count
+    , status
+    , release_date
+    , revenue
+    , runtime
+    , budget
+    , imdb_id AS imdb_movie_id
+    , popularity AS tmdb_popularity
+    , genres AS tmdb_genres
+  FROM source
+)
+
 SELECT *
-FROM {{ source('tmdb', 'tmdb_movie_dataset') }}
+FROM rename_column
