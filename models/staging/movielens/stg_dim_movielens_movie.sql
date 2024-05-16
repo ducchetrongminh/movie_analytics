4ac60f782d68be790f50d762643a935dd75c5bfc
Duc Che <ducchetrongminh@gmail.com>
2024-05-16 13:48:01 +0700

    use new key

--- models/staging/movielens/stg_dim_movielens_movie.sql
+++ models/staging/movielens/stg_dim_movielens_movie.sql
@@ -4,7 +4,7 @@ WITH join_data AS (
     , dim_movielens_movie__link.imdb_movie_id
     , dim_movielens_movie__link.tmdb_movie_id
   FROM {{ ref('base_dim_movielens_movie') }} AS dim_movielens_movie 
-  JOIN {{ ref('base_dim_movielens_movie__link') }} AS dim_movielens_movie__link 
+  JOIN {{ ref('stg_dim_movielens_movie__link') }} AS dim_movielens_movie__link 
     USING (movielens_movie_id)
 )
 
