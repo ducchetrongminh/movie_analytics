891f09f51fb8ebb0eaeefc9ca5cdedf9e2c227e8
Duc Che <ducchetrongminh@gmail.com>
2024-05-16 13:46:59 +0700

    rename to integer id

--- models/base/movielens/base_dim_movielens_movie__link.sql
+++ models/base/movielens/base_dim_movielens_movie__link.sql
@@ -6,7 +6,7 @@ WITH source AS (
 , rename_column AS (
   SELECT 
     movieId AS movielens_movie_id
-    , imdbId AS imdb_movie_id
+    , imdbId AS imdb_movie_integer_id
     , tmdbId AS tmdb_movie_id
   FROM source 
 )
@@ -14,7 +14,7 @@ WITH source AS (
 , cast_type AS (
   SELECT 
     CAST(movielens_movie_id AS INTEGER) AS movielens_movie_id
-    , CAST(imdb_movie_id AS INTEGER) AS imdb_movie_id
+    , CAST(imdb_movie_integer_id AS INTEGER) AS imdb_movie_integer_id
     , CAST(tmdb_movie_id AS INTEGER) AS tmdb_movie_id
   FROM rename_column
 )
@@ -22,13 +22,13 @@ WITH source AS (
 , handle_null AS (
   SELECT 
     movielens_movie_id 
-    , COALESCE(imdb_movie_id, 0) AS imdb_movie_id
+    , COALESCE(imdb_movie_integer_id, 0) AS imdb_movie_integer_id
     , COALESCE(tmdb_movie_id, 0) AS tmdb_movie_id
   FROM cast_type
 )
 
 SELECT 
   movielens_movie_id
-  , imdb_movie_id
+  , imdb_movie_integer_id
   , tmdb_movie_id
 FROM handle_null
