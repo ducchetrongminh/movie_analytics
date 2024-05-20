f1457b997690b5b01d3b1112db9defda88075113
Duc Che <ducchetrongminh@gmail.com>
2024-05-20 11:26:43 +0700

    rename to movielens title to extract

--- models/base/movielens/base_dim_movielens_movie.sql
+++ models/base/movielens/base_dim_movielens_movie.sql
@@ -6,7 +6,7 @@ WITH source AS (
 , rename_column AS (
   SELECT 
     movieId AS movielens_movie_id
-    , title
+    , title AS movielens_title
     , genres AS movielens_genres
   FROM source 
 )
@@ -14,7 +14,7 @@ WITH source AS (
 , cast_type AS (
   SELECT 
     CAST(movielens_movie_id AS INTEGER) AS movielens_movie_id
-    , CAST(title AS STRING) AS title 
+    , CAST(movielens_title AS STRING) AS movielens_title 
     , CAST(movielens_genres AS STRING) AS movielens_genres
   FROM rename_column
 )
@@ -22,13 +22,13 @@ WITH source AS (
 , handle_null AS (
   SELECT 
     movielens_movie_id
-    , title
+    , movielens_title
     , COALESCE(NULLIF(movielens_genres, '(no genres listed)'), 'Undefined') AS movielens_genres
   FROM cast_type
 )
 
 SELECT 
   movielens_movie_id
-  , title
+  , movielens_title
   , movielens_genres
 FROM handle_null
