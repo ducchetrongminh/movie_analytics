057f5ad7a668104016b518aac4564ed9654a265a
Duc Che <ducchetrongminh@gmail.com>
2024-05-20 11:29:54 +0700

    extract year and title

--- models/staging/movielens/stg_dim_movielens_movie.sql
+++ models/staging/movielens/stg_dim_movielens_movie.sql
@@ -1,4 +1,12 @@
-WITH dim_movielens_movie__rating AS (
+WITH extract_year AS (
+  SELECT 
+    *
+    , REGEXP_REPLACE(movielens_title, r'(\s\([0-9]+\))$', '') AS title
+    , REGEXP_EXTRACT(movielens_title, r'\s\(([0-9]+)\)$') AS release_year
+  FROM {{ ref('base_dim_movielens_movie') }}
+)
+
+, dim_movielens_movie__rating AS (
   SELECT 
     movielens_movie_id
     , AVG(rating) AS movielens_rating_avg
@@ -14,7 +22,7 @@ WITH dim_movielens_movie__rating AS (
     , dim_movielens_movie__link.tmdb_movie_id
     , dim_movielens_movie__rating.movielens_rating_avg
     , dim_movielens_movie__rating.movielens_rating_count
-  FROM {{ ref('base_dim_movielens_movie') }} AS dim_movielens_movie 
+  FROM extract_year AS dim_movielens_movie 
   JOIN {{ ref('stg_dim_movielens_movie__link') }} AS dim_movielens_movie__link 
     USING (movielens_movie_id)
   LEFT JOIN dim_movielens_movie__rating
@@ -25,6 +33,8 @@ SELECT
   movielens_movie_id
   , title
   , movielens_genres
+
+  , release_year
 
   , movielens_rating_avg 
   , movielens_rating_count
