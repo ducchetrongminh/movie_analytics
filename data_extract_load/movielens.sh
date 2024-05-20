1da7e38acc10cd9c02a503f4a2e40ca5b00d9fd8
Duc Che <ducchetrongminh@gmail.com>
2024-05-20 10:25:18 +0700

    load movielens latest

--- data_extract_load/movielens.sh
+++ data_extract_load/movielens.sh
@@ -1,26 +1,26 @@
-# Create dataset movielens_25m and upload data
+# Create dataset movielens and upload data
 # To run this script, on Google Cloud Platform, Activate Cloud Shell
 # Activate Cloud Shell: https://cloud.google.com/shell/docs/using-cloud-shell#start_a_new_session
 
-curl -O 'http://files.grouplens.org/datasets/movielens/ml-25m.zip'
-unzip ml-25m.zip
+curl -O 'http://files.grouplens.org/datasets/movielens/ml-latest.zip'
+unzip ml-latest.zip
 
-bq --location US mk --dataset movielens_25m
-
-bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
-  movielens_25m.genome_scores ml-25m/genome-scores.csv
+bq --location US mk --dataset movielens
 
 bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
-  movielens_25m.genome_tags ml-25m/genome-tags.csv
+  movielens.genome_scores ml-latest/genome-scores.csv
 
 bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
-  movielens_25m.links ml-25m/links.csv
+  movielens.genome_tags ml-latest/genome-tags.csv
 
 bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
-  movielens_25m.movies ml-25m/movies.csv
+  movielens.links ml-latest/links.csv
 
 bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
-  movielens_25m.ratings ml-25m/ratings.csv
+  movielens.movies ml-latest/movies.csv
 
 bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
-  movielens_25m.tags ml-25m/tags.csv
+  movielens.ratings ml-latest/ratings.csv
+
+bq load --source_format=CSV --autodetect --skip_leading_rows=1 \
+  movielens.tags ml-latest/tags.csv
