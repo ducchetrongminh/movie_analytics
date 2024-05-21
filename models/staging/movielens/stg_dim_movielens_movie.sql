5d76bd47828d3fb00dd20424c089842ccd6dd726
Duc Che <ducchetrongminh@gmail.com>
2024-05-21 18:36:30 +0700

    fix bug reversed movielens title

--- models/staging/movielens/stg_dim_movielens_movie.sql
+++ models/staging/movielens/stg_dim_movielens_movie.sql
@@ -6,6 +6,22 @@ WITH extract_year AS (
   FROM {{ ref('base_dim_movielens_movie') }}
 )
 
+, cleanse_title AS (
+  /*
+  CONTEXT: We want to map movies from different platforms based on (title+year) (see docs\doc_movie_mapping.md for more info).
+  PROBLEM: Many Movielens titles are in reversed order (e.g. `Dark Knight Rises, The` instead of `The Dark Knight Rises`)
+  SOLUTION: We reverse `The` to the front.
+  */
+  SELECT 
+    * EXCEPT (title)
+    , CASE 
+      WHEN title LIKE '%, The'
+      THEN CONCAT('The ', REGEXP_REPLACE(title, r', The$', ''))
+      ELSE title END
+    AS title
+  FROM extract_year
+)
+
 , join_data AS (
   SELECT 
     dim_movielens_movie.*
@@ -13,7 +29,7 @@ WITH extract_year AS (
     , dim_movielens_movie__link.tmdb_movie_id
     , dim_movielens_movie__rating.movielens_rating_avg
     , dim_movielens_movie__rating.movielens_rating_count
-  FROM extract_year AS dim_movielens_movie 
+  FROM cleanse_title AS dim_movielens_movie 
   JOIN {{ ref('stg_dim_movielens_movie__link') }} AS dim_movielens_movie__link 
     USING (movielens_movie_id)
   LEFT JOIN {{ ref('stg_dim_movielens_movie__rating') }} AS dim_movielens_movie__rating
